cdd424765a4de6f9614be337d526f840ba7410e9
github-action-benchmark <github@users.noreply.github.com>
2020-12-13 21:47:37 +0000

    add OpenTelemetry Python Benchmarks - Python 3.7 - (pytest) benchmark result for 2f1f13f96742284ba3e86f413148e80fe4988686

---
+++ benchmarks/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1607896056822,
+  "repoUrl": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python",
+  "entries": {
+    "OpenTelemetry Python Benchmarks - Python 3.7 -": [
+      {
+        "commit": {
+          "author": {
+            "email": "enowell@amazon.com",
+            "name": "(Eliseo) Nathaniel Ruiz Nowell",
+            "username": "NathanielRN"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2f1f13f96742284ba3e86f413148e80fe4988686",
+          "message": "Use gh-pages to save performance benchmarks results (#1469)",
+          "timestamp": "2020-12-13T12:05:16-08:00",
+          "tree_id": "5bd658914eea3e94fa977897bf34fa99db03fd90",
+          "url": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python/commit/2f1f13f96742284ba3e86f413148e80fe4988686"
+        },
+        "date": 1607896055493,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 26225.955072437337,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000004794299147200131",
+            "extra": "mean: 38.13016522136 usec\nrounds: 4497"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 20200.491140695958,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000016945571184402555",
+            "extra": "mean: 49.503746866104535 usec\nrounds: 7419"
+          }
+        ]
+      }
+    ]
+  }
+}
