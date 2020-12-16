4a6ac40ac5441a44fa632de9a13b9d4737c1de3b
github-action-benchmark <github@users.noreply.github.com>
2020-12-16 10:26:00 +0000

    add OpenTelemetry Python Benchmarks - Python 3.6 - core (pytest) benchmark result for 9cfe5559ce6c015eea04f66fb9a4ece596f775b3

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1608114356887,
+  "lastUpdate": 1608114360221,
   "repoUrl": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 -": [
@@ -356,6 +356,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000027191402737396514",
             "extra": "mean: 72.20750011096452 usec\nrounds: 4501"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rob.knox.dev@gmail.com",
+            "name": "Rob Knox",
+            "username": "robwknox"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "9cfe5559ce6c015eea04f66fb9a4ece596f775b3",
+          "message": "Adding support for array attributes to Zipkin exporter (#1285)",
+          "timestamp": "2020-12-15T20:38:24-08:00",
+          "tree_id": "e9a7b62d8a75ebd757b49b580a4debc56e5f8bd2",
+          "url": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python/commit/9cfe5559ce6c015eea04f66fb9a4ece596f775b3"
+        },
+        "date": 1608114357540,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 22868.480596028734,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000014415806523547366",
+            "extra": "mean: 43.72830961815875 usec\nrounds: 1518"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 16239.309503401364,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000017757049816819936",
+            "extra": "mean: 61.57897291079694 usec\nrounds: 6091"
           }
         ]
       }
