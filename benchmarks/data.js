32f47fc23a4b16bf6e2d1748b42f0a85ca4c1ddd
github-action-benchmark <github@users.noreply.github.com>
2021-01-04 17:58:26 +0000

    add OpenTelemetry Python Benchmarks - Python 3.9 - core (pytest) benchmark result for bd8db6e2a461d896c1c0556efd0f4b569106fa96

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609783102947,
+  "lastUpdate": 1609783106632,
   "repoUrl": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 -": [
@@ -890,6 +890,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000062341412201060995",
             "extra": "mean: 39.99906964580954 usec\nrounds: 7452"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srikanth.chekuri92@gmail.com",
+            "name": "Srikanth Chekuri",
+            "username": "lonewolf3739"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bd8db6e2a461d896c1c0556efd0f4b569106fa96",
+          "message": "Add protobuf via gRPC exporting support for Jaeger (#1471)",
+          "timestamp": "2021-01-04T09:13:16-08:00",
+          "tree_id": "ab66aeae2640c477e711409223dda795b083ee77",
+          "url": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python/commit/bd8db6e2a461d896c1c0556efd0f4b569106fa96"
+        },
+        "date": 1609783101716,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 32168.299425247962,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000003708874300815825",
+            "extra": "mean: 31.086504971261522 usec\nrounds: 5230"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 23671.10042454502,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000004780463801679803",
+            "extra": "mean: 42.24560675527703 usec\nrounds: 7283"
           }
         ]
       }
