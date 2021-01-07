53401298661a15c605a23198ec19cc68ca50d53b
github-action-benchmark <github@users.noreply.github.com>
2021-01-07 05:36:23 +0000

    add OpenTelemetry Python Benchmarks - Python 3.5 - exporter (pytest) benchmark result for eb7125d7a7fb42dde67088e055c6053ea6c103b2

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1609997777829,
+  "lastUpdate": 1609997783556,
   "repoUrl": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 -": [
@@ -2456,6 +2456,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0007799419612561729",
             "extra": "mean: 271.1844331210562 usec\nrounds: 4239"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "aboten@lightstep.com",
+            "name": "alrex",
+            "username": "codeboten"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "eb7125d7a7fb42dde67088e055c6053ea6c103b2",
+          "message": "Pass OTLP metadata as tuple instead of string (#1507)",
+          "timestamp": "2021-01-06T21:14:55-08:00",
+          "tree_id": "661f947040e0d995d408e9069820b011ae28f561",
+          "url": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python/commit/eb7125d7a7fb42dde67088e055c6053ea6c103b2"
+        },
+        "date": 1609997776020,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_simple_span_processor",
+            "value": 1995.0052285128818,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00012133318444909289",
+            "extra": "mean: 501.2518191470709 usec\nrounds: 94"
+          },
+          {
+            "name": "exporter/opentelemetry-exporter-otlp/tests/performance/benchmarks/test_benchmark_trace_exporter.py::test_batch_span_processor",
+            "value": 2977.351848239077,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0009024884609186307",
+            "extra": "mean: 335.8689368847821 usec\nrounds: 4167"
           }
         ]
       }
