ae1c73e093f4a475ce98c40ae8223b2c0000c2c4
github-action-benchmark <github@users.noreply.github.com>
2021-01-07 18:12:23 +0000

    add OpenTelemetry Python Benchmarks - Python pypy3 - core (pytest) benchmark result for cd39fc17b21baf68eddc4ddd0acfe2e80df59eae

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1610043063542,
+  "lastUpdate": 1610043143277,
   "repoUrl": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 -": [
@@ -2300,6 +2300,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000006225733379012866",
             "extra": "mean: 5.852740826256927 usec\nrounds: 103093"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "srikanth.chekuri92@gmail.com",
+            "name": "Srikanth Chekuri",
+            "username": "lonewolf3739"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cd39fc17b21baf68eddc4ddd0acfe2e80df59eae",
+          "message": "Update Jaeger exporter status code (#1488)",
+          "timestamp": "2021-01-07T08:05:29-08:00",
+          "tree_id": "2d2eb6aea5add7398b0ba85fe8a38da6ff809abb",
+          "url": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python/commit/cd39fc17b21baf68eddc4ddd0acfe2e80df59eae"
+        },
+        "date": 1610043141722,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 231791.53982152307,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00005240489687108181",
+            "extra": "mean: 4.3142213075161795 usec\nrounds: 153847"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 154841.235759587,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000007907569664497244",
+            "extra": "mean: 6.458227971989594 usec\nrounds: 99010"
           }
         ]
       }
