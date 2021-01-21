7ffc040cf151b86ff441e4763080dc9e53f36036
github-action-benchmark <github@users.noreply.github.com>
2021-01-21 20:40:25 +0000

    add OpenTelemetry Python Benchmarks - Python 3.6 - core (pytest) benchmark result for b32365bf71a8a258e668bd81e90f158f2a88a4d7

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1611261620627,
+  "lastUpdate": 1611261625806,
   "repoUrl": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python",
   "entries": {
     "OpenTelemetry Python Benchmarks - Python 3.7 -": [
@@ -1762,6 +1762,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000002000223115095751",
             "extra": "mean: 73.56633786671888 usec\nrounds: 5147"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anton.ryzhov@zalando.de",
+            "name": "Anton Ryzhov",
+            "username": "anton-ryzhov"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b32365bf71a8a258e668bd81e90f158f2a88a4d7",
+          "message": "Allow to `start_as_current_span` with `end_on_exit=False` (#1519)",
+          "timestamp": "2021-01-21T10:01:46-08:00",
+          "tree_id": "8bdccaf49d5c64062a1ba7a787e890da2a104842",
+          "url": "https://github.com/Vinta-IAAS-Labs/opentelemetry-python/commit/b32365bf71a8a258e668bd81e90f158f2a88a4d7"
+        },
+        "date": 1611261623033,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_span",
+            "value": 18621.635012613653,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000009093029231270773",
+            "extra": "mean: 53.7009773482637 usec\nrounds: 1810"
+          },
+          {
+            "name": "opentelemetry-sdk/tests/performance/benchmarks/trace/test_benchmark_trace.py::test_simple_start_as_current_span",
+            "value": 12209.9993413934,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00001788647754069669",
+            "extra": "mean: 81.90008631776719 usec\nrounds: 7148"
           }
         ]
       }
